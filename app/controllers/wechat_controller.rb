3307535a6384ff8ed6bd5272d4a4657e92e57cda
king <er6788@126.com>
2016-10-21 13:45:01 +0800

    new rep

---
+++ app/controllers/wechat_controller.rb
@@ -0,0 +1,26 @@
+class WechatController < ApplicationController
+layout false
+
+  def create
+    body = Hash.from_xml(request.body.read)["xml"]
+    if request.request_method == "POST" && verify_wechat_auth(ENV["AUTHTOKEN"], body["Encrypt"])
+      render text: 'success'
+    end
+  end
+
+  def share
+  end
+
+  private
+
+  def wechat_message_params
+    params.permit(:timestamp, :nonce, :msg_signature)
+  end
+
+  def verify_wechat_auth(token, encrypt)
+    return unless token
+    arr = [ token, wechat_message_params[:timestamp],
+            wechat_message_params[:nonce], encrypt ].sort
+    Digest::SHA1.hexdigest(arr.join) == wechat_message_params["msg_signature"]
+  end
+end
---
+++ config/weixin_authorize.rb
@@ -0,0 +1,21 @@
+# # Give a special namespace as prefix for Redis key, when your have more than one project used weixin_authorize, this config will make them work fine.
+# redis = Redis::Namespace.new("#{namespace}", :redis => redis)
+
+
+
+unless Rails.env.development?
+ 
+redis = Redis.new(:host => "127.0.0.1", :port => "6379", :db => 15)
+
+ # 每次重启时，会把当前的命令空间所有的access_token 清除掉。
+# exist_keys = redis.keys("#{namespace}:*")
+exist_keys = redis.keys("*")
+exist_keys.each{|key|redis.del(key)}
+
+  WeixinAuthorize.configure do |config|
+    config.redis = redis
+  end
+
+  WECHAT_CLIENT ||= WeixinAuthorize::Client.new(ENV["WECHATID"], ENV["WECHATSECRET"])
+
+end
