357862ad680543ee810e6a72a65efac3556624da
king <er6788@126.com>
2016-10-21 16:33:44 +0800

    subscribe

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -5,6 +5,7 @@ layout false
   	if request.request_method == "POST" && verify_wechat_auth 
        info = Hash.from_xml(request.body.read)["xml"]
        if  info[:MsgType] == "event" && info[:Event] == "subscribe" 
+      message = "欢迎,最新活动&lt;a href='#{ENV["TESTURL"]}'&gt; 去看看,&lt;/a&gt;,详情猛戳查看"
        	render :xml => teplate_xml(message，info)
        end
   	elsif request.request_method == "GET" && tmp_encrypted_str == signature
@@ -43,4 +44,5 @@ layout false
 Text
     template_xml
   end
+
 end
