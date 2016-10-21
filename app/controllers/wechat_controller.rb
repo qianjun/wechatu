f9a207dff0679e12fbd50845ef5ec25cbdffb013
king <er6788@126.com>
2016-10-21 16:17:27 +0800

    sunbscribe

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -2,9 +2,14 @@ class WechatController < ApplicationController
 layout false
  
   def connection
-    if verify_wechat_auth
-       render text: wechat_params["echostr"]
-    end
+  	if request.request_method == "POST" && verify_wechat_auth 
+       info = Hash.from_xml(request.body.read)["xml"]
+       if  info[:MsgType] == "event" && info[:Event] == "subscribe" 
+       	render :xml => teplate_xml(message，info)
+       end
+  	elsif request.request_method == "GET" && tmp_encrypted_str == signature
+  		render :text => tmp_encrypted_str == signature ? wechat_params["echostr"] :  false
+  	end
   end
 
   def share
@@ -21,5 +26,21 @@ layout false
     arr = [ ENV["AUTHTOKEN"], wechat_params[:timestamp],
             wechat_params[:nonce] ].sort
     Digest::SHA1.hexdigest(arr.join) == wechat_params["signature"]
+  end
+
+  #文本回复模板
+  def teplate_xml(message,info)
+    template_xml =
+      <<Text
+          <xml>
+            <ToUserName><![CDATA[#{info[:FromUserName]}]]></ToUserName>
+            <FromUserName><![CDATA[#{info[:ToUserName]}]]></FromUserName>
+            <CreateTime>#{Time.now.to_i}</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content>#{message}</Content>
+            <FuncFlag>0</FuncFlag>
+          </xml>
+Text
+    template_xml
   end
 end
