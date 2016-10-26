5ff95bd379bea25edcd01bcf4ba63d732ecc61b8
king <er6788@126.com>
2016-10-26 15:25:50 +0800

    tickect

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "wechat#index"
   match 'connection', to: 'wechat#connection',via: [:get,:post]
-  match 'mp_ticket', to: 'wechat#mp_ticket', via: [:get]
+  match 'mp_ticket', to: 'wechat#mp_ticket', via: [:get,:post]
   resources :wechat 
  
 end
