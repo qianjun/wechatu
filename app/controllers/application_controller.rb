a007adbcd46877a386fe31b0709a60e8287c7d86
king <er6788@126.com>
2016-11-22 16:28:27 +0800

    111'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
  def auth_weixin
-    return redirect_to(WECHAT_CLIENT.authorize_url(request.original_url, "snsapi_base"))
+    return redirect_to(WECHAT_CLIENT.authorize_url(request.original_url, "snsapi_base")) unless params[:code].present?
     sns_info = WECHAT_CLIENT.get_oauth_access_token(params[:code])
     return redirect_to(:error) if sns_info.code != 0
     _result = sns_info.result
